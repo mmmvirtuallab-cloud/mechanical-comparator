e746166beea52f775811da4dc67459ca22942d36
ismail <ismailisims1@gmail.com>
2025-12-23 23:30:58 +0530

    Initial commit

---
+++ js/intro_data.js
@@ -0,0 +1,28 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "Mechanical Comparator",
+    
+    // Button Text & Link
+    buttonLabel: "Experiment",
+    buttonLink: "lab.html",
+
+    // Main Page Content
+    mainHeading: "Mechanical Comparator",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To understand the construction and working of Mechanical Comparator.",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "HARSHITA - 2023507005",
+        "SHAKTHI - 2023507007",
+        "VISHNUVARDHINI - 2023507043"
+    ]
+};
